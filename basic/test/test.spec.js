3f73dc46a300e86dfc8d1aac57820b5ca0ffc293
Jarmo Koivisto <jarmo.kvst@gmail.com>
2016-06-14 18:26:26 +0300

    replace chai with karma-chai, add sinon

--- basic/test/test.spec.js
+++ basic/test/test.spec.js
@@ -1,12 +1,10 @@
 
-var chai = require('chai')
-
 var moduleTest = require("../src/module.js");
-
-chai.should();
+var sinon = require("sinon");
 
 describe("sample test", function() {
     it("test 1", function() {
+        var spy = sinon.spy();
         moduleTest().should.equal("test");
     });
 });
